3088a03ec0f48847e213e361c1ce67da07114de6
RathikumarJanushan <janushanr17@gmail.com>
2022-04-06 17:18:56 +0530

    1st commit

---
+++ Hydroponic-system-electronic/Hydroponic-system-electronic.ino
@@ -0,0 +1,65 @@
+#include <dht.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);  
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+
+
+#define dht_apin A0 // Analog Pin sensor is connected to
+
+dht DHT;
+
+void setup() {
+
+   sensors.begin();  // Start up the library
+  Serial.begin(9600);
+
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+
+}//end "setup()"
+
+void loop() {
+  //Start of Program
+
+  DHT.read11(dht_apin);
+
+  Serial.print("Current humidity = ");
+  Serial.print(DHT.humidity);
+  Serial.print("%  ");
+  Serial.print("temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.println("C  ");
+
+  delay(5000);//Wait 5 seconds before accessing sensor again.
+
+  //Fastest should be once every two seconds.
+
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+
+  //print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print((char)176);//shows degrees character
+  Serial.print("C  |  ");
+  
+  //print the temperature in Fahrenheit
+  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
+  Serial.print((char)176);//shows degrees character
+  Serial.println("F");
+  
+  delay(500);
+
+}// end loop(
